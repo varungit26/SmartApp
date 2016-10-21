c707cb2e938399ba63a64ba924ff061e5f0e33ea
varungit26 <varunvidala@gmail.com>
2016-10-21 19:44:51 -0400

    Initial Commit

---
+++ src/main/java/com/techresource/controller/MainController.java
@@ -0,0 +1,16 @@
+package com.techresource.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class MainController {
+
+	/*@RequestMapping(value = { "", "/", "welcome" })
+	public ModelAndView welcome() {
+
+		ModelAndView model = new ModelAndView("welcome");
+		return model;
+	}*/
+}
